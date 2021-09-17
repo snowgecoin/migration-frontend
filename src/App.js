036ee98a6c22bbd5dbd1f6ad4c4ee1a8a0481c85
snowgecoin <snowgecoin@gmail.com>
2021-09-17 17:48:36 -0500

    bye gatsby

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import * as React from "react"
+import './scss/main.scss'
+import TextLoop from "react-text-loop";
+import Particles from 'react-particles-js';
+import ConnectButton from "./components/ConnectButton";
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  alignContent: "center",
+  alignItems: "center",
+  justifyContent: "center",
+  flex: "flex"
+}
 
 function App() {
+
+  const [shineAnimationTitle, setShineAnimationTitle] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pageContainer">
+      <Particles
+        style={{position: 'absolute'}}
+        params={{
+          "particles": {
+              "number": {
+                  "value": 160,
+                  "density": {
+                      "enable": false
+                  }
+              },
+              "size": {
+                  "value": 10,
+                  "random": true
+              },
+              "move": {
+                  "direction": "bottom",
+                  "out_mode": "out",
+                  "bounce": false
+              },
+              "line_linked": {
+                  "enable": false
+              }
+          }
+      }} />
+      <main style={pageStyles}>
+        <title>SnowgeCoin Re-floofinator</title>
+        <div className={`title ${shineAnimationTitle ? 'title-animated' : ''}`} onMouseEnter={() => setShineAnimationTitle(() => true)} onAnimationEnd={() => setShineAnimationTitle(() => false)}>
+          SnowgeCoin <TextLoop nowrap={false} interval={[3000,3000,6000]} springConfig={{ stiffness: 180, damping: 12}}><span>Upgrader</span><span>Migrator</span><span>Refloofinator</span></TextLoop>
+        </div>
+        <div class="container">
+          <div class="loader"></div>
+          <div className="in-loader">
+            Coming Soon
+            {/* <ConnectButton/> */}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DAppProvider } from "@usedapp/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={{}}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
