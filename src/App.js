de41eb0b52f46a722057d29dc3780eab1724e259
snowgecoin <snowgecoin@gmail.com>
2021-10-06 01:07:20 -0500

    Approve button

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ import TextLoop from "react-text-loop";
 import Particles from 'react-particles-js';
 import ConnectButton from "./components/ConnectButton";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-  alignContent: "center",
-  alignItems: "center",
-  justifyContent: "center",
-  flex: "flex"
-}
-
 function App() {
 
   const [shineAnimationTitle, setShineAnimationTitle] = React.useState(false);
@@ -45,7 +35,7 @@ function App() {
               }
           }
       }} />
-      <main style={pageStyles}>
+      <div className="main">
         <title>SnowgeCoin Re-floofinator</title>
         <div className={`title ${shineAnimationTitle ? 'title-animated' : ''}`} onMouseEnter={() => setShineAnimationTitle(() => true)} onAnimationEnd={() => setShineAnimationTitle(() => false)}>
           SnowgeCoin <TextLoop nowrap={false} interval={[3000,3000,6000]} springConfig={{ stiffness: 180, damping: 12}}><span>Upgrader</span><span>Migrator</span><span>Refloofinator</span></TextLoop>
@@ -56,7 +46,7 @@ function App() {
             <ConnectButton/>
           </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -1,32 +1,89 @@
 import * as React from 'react';
 import './ConnectButton.scss';
 import CountUp from 'react-countup';
-import { useEthers, useEtherBalance, useTokenBalance } from "@usedapp/core";
+import { useEthers, useEtherBalance, useTokenBalance, useContractFunction } from "@usedapp/core";
+import SnowgeCoinJSON from '../abis/SnowgeCoin.json';
+import { utils } from 'ethers';
+import { BigNumber} from "@ethersproject/bignumber";
+import { Contract } from '@ethersproject/contracts'
 import { formatUnits } from "@ethersproject/units";
+
+const easingFn = (t, b, c, d) => {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t * t * t + 2) + b;
+};
 
 export default function ConnectButton() {
   const SNOWGE_V1 = '0x5e9280d53f28281ce098c8f64e49f5f5dc9ea185';
+  const REFLOOFINATOR_V1 = '0x10ED43C718714eb63d5aA57B78B54704E256024E'; // currently just PCS until refloofinator gets deployed
   const {activateBrowserWallet, account } = useEthers();
   const etherBalance = useEtherBalance(account);
   const oldTokenBalance = useTokenBalance(SNOWGE_V1, account);
-  const [snowgeBalance, setSnowgeBalance] = React.useState(5000000);
+  const SnowgeABI = [
+        // balanceOf
+        {
+            "constant": true,
+            "inputs": [{"name": "_owner", "type": "address"}],
+            "name": "balanceOf",
+            "outputs": [{"name": "balance", "type": "uint256"}],
+            "type": "function"
+        },
+        // decimals
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "decimals",
+            "outputs": [{"name": "", "type": "uint8"}],
+            "type": "function"
+        },
+        //approve
+        {
+             "inputs":[
+                {
+                   "internalType":"address",
+                   "name":"spender",
+                   "type":"address"
+                },
+                {
+                   "internalType":"uint256",
+                   "name":"amount",
+                   "type":"uint256"
+                }
+             ],
+             "name":"approve",
+             "outputs":[
+                {
+                   "internalType":"bool",
+                   "name":"",
+                   "type":"bool"
+                }
+             ],
+             "stateMutability":"nonpayable",
+             "type":"function"
+        }
+    ];
+  console.log(SnowgeABI)
+  const oldTokenContract = new Contract(SNOWGE_V1, SnowgeABI);
+  const approveContractFunction = useContractFunction(oldTokenContract, 'approve', { transactionName: 'Approve Migration' });
 
   function handleConnectWallet() {
     activateBrowserWallet();
   }
 
-  function updateSnowgeBalance() {
-    console.log(formatUnits(oldTokenBalance, 9));
-    console.log(snowgeBalance);
-    setSnowgeBalance(formatUnits(oldTokenBalance, 9));
+  const handleApprove = () => {
+    approveContractFunction.send(REFLOOFINATOR_V1, BigNumber.from('1000000000000000000000000000000000000000000000000000'))
   }
+
+
 
   return account ? (
     <div>
       <p className={'balance-title'}>Snowge Balance</p>
-      <p className={'balance-number'}><CountUp start={0} end={snowgeBalance} separator=',' decimals={4} suffix=" tokens"/></p>
-      <p className={'connected'}>Connected</p>
-      <button className="snowgebutton" onClick={updateSnowgeBalance}>refresh</button>
+      <p className={'balance-number'}>{oldTokenBalance && <CountUp start={0} end={parseFloat(formatUnits(oldTokenBalance, 9))} separator={','} decimals={2} easingFn={easingFn} duration={2} suffix=" tokens"/>}</p>
+      <p className={'connected'}>🟢 {account.substring(0,2) + '...' + account.substring(39)} | Connected</p>
+      <button className="snowgebutton" onClick={handleApprove}>approve</button>
     </div>
   ) : (
     <button className="snowgebutton" onClick={handleConnectWallet}>Connect to a Wallet</button>
